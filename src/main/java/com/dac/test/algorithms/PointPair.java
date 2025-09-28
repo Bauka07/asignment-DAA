8b729e575782e37218f860b5e3e69491257e89b9
bauka <155758258+Bauka07@users.noreply.github.com>
2025-09-28 23:42:41 +0500

    Update PointPair.java

--- src/main/java/com/dac/test/algorithms/PointPair.java
+++ src/main/java/com/dac/test/algorithms/PointPair.java
@@ -1,8 +1,6 @@
+// File: src/main/java/com/dac/algorithms/PointPair.java
 package com.dac.algorithms;
 
-/**
- * Represents a pair of points and their distance.
- */
 public class PointPair {
     public final Point p1, p2;
     public final double distance;
@@ -13,8 +11,21 @@ public class PointPair {
         this.distance = p1.distanceTo(p2);
     }
     
+    public double getDistance() {
+        return distance;
+    }
+    
     @Override
     public String toString() {
-        return String.format("PointPair{%s, %s, distance=%.6f}", p1, p2, distance);
+        return String.format("PointPair{%s <-> %s, distance=%.6f}", p1, p2, distance);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        PointPair pair = (PointPair) obj;
+        return ((p1.equals(pair.p1) && p2.equals(pair.p2)) || 
+                (p1.equals(pair.p2) && p2.equals(pair.p1)));
     }
 }
